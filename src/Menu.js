3ea0f9ebe5b5548a082a8ecc46036f5edaeac549
Nick Kinlen <nick_kinlen@hotmail.com>
2018-07-24 01:26:07 -0400

    Added png logo with name to header

--- src/Menu.js
+++ src/Menu.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Logo from './images/centered.png';
+import SpaceStations from './images/space.png';
+
+
 
 const NavMenu = () => {
   return (
     <div className="menu-div">
+      <div className="logo-container">
+        <img src={Logo} height="100%" width="175"/>
+      </div>
       <ul className="nav">
         <li>
           <a href="#intro">About</a>
