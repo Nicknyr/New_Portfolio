2bc0f80b4c196b25232b69c730d9c984e4444e2d
Nick <nick_kinlen@hotmail.com>
2018-09-27 21:33:25 -0400

    Ditched Formspree and switched to FormCarry for the contact form. Form is set up and properly submits

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,41 +1,46 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 
 class ContactForm extends Component {
-  constructor(props) {
-    super(props);
+  constructor(props){
+   super(props);
+   this.state = {name: "", surname: "", email: "", message: ""};
+ }
 
-    this.state = {
-      value: ''
-    };
-  }
+ handleForm = e => {
+   axios.post(
+     "https://formcarry.com/s/VphZwAcq6nJ",
+     this.state,
+     {headers: {"Accept": "application/json"}}
+     )
+     .then(function (response) {
+       console.log(response);
+     })
+     .catch(function (error) {
+       console.log(error);
+     });
 
-  handleSubmit = (event) => {
-    //alert("Form submitted");
-    //event.preventDefault();
-  }
+   e.preventDefault();
+ }
 
-  handleChange= (event) => {
-    this.setState({
-      value: event.target.value
-    });
-  }
+ handleFields = e => this.setState({ [e.target.name]: e.target.value });
 
 
   render() {
     return (
       <div className="form">
         <div className="form-container">
-          <form action="https://formspree.io/nick_kinlen@hotmail.com" method="POST">
+          <form action="https://formcarry.com/s/VphZwAcq6nJ" method="POST" accept-charset="UTF-8" >
               <label className="name">
-                <input type="text" placeholder="First name" name="first_name" />
+                <input type="text" placeholder="First name" name="firstName" />
               </label>
               <label className="name">
-                <input type="text" placeholder="Last name" name="last_name" />
+                <input type="text" placeholder="Last name" name="lastName" />
               </label>
               <label className="email">
-                <input type="text" placeholder="Email" name="_replyto" />
+                <input type="text" placeholder="Email" name="email" />
               </label>
+              <input type="hidden" name="_gotcha" />
             <div className="message-box">
               <textarea value={this.state.value} onChange={this.handleChange} placeholder="Your message" rows="7" cols="50" name="message"></textarea>
             </div>
